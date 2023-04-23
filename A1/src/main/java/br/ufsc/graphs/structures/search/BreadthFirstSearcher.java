7ac4633b39824968b5ec2c2daab20156d5dd6ccd
anthongretter <anthonporathgretter@yahoo.com.br>
2023-04-23 11:05:09 -0300

    versao final da busca em largura
    * adapta busca em largura com a Streams API e, consequentemente, resolve problema de logs incorretos com o formato proposto
    * cria SearchLogger para centralizar logs de buscas

--- A1/src/main/java/br/ufsc/graphs/structures/search/BreadthFirstSearcher.java
+++ A1/src/main/java/br/ufsc/graphs/structures/search/BreadthFirstSearcher.java
@@ -2,44 +2,47 @@ package br.ufsc.graphs.structures.search;
 
 import br.ufsc.graphs.structures.Graph;
 import br.ufsc.graphs.structures.WeightedGraphImp;
+import br.ufsc.graphs.utils.SearchLogger;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class BreadthFirstSearcher implements GraphSearcher {
 
     public static Pair<List<Integer>, List<Integer>> search(Graph graph, int initialV) {
+        SearchLogger logger = new SearchLogger(0);
+
+        // configura todos os vertices
         boolean[] visitArray = new boolean[graph.getVerticesQnt()];
         List<Integer> distances = new ArrayList<>(Collections.nCopies(graph.getVerticesQnt(), (int) WeightedGraphImp.NULL_VALUE));
         List<Integer> predecessors = new ArrayList<>(List.of(initialV));
 
+        // configura vertice de origem
         visitArray[initialV] = true;
         distances.set(initialV, 0);
 
-        Queue<Integer> queue = new LinkedList<>(List.of(initialV));
-        int level = 0;
+        // preparando fila de visitas
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(initialV);
 
+        logger.log(predecessors);
+
+        // propagacao das visitas
         while (!queue.isEmpty()) {
-            log(level++, queue.stream().toList());
             int unqueued = queue.remove();
-            for (int neighbor : graph.neighbours(unqueued)) {
-                if (!visitArray[neighbor]) {
-                    visitArray[neighbor] = true;
-                    distances.set(neighbor, distances.get(unqueued) + 1);
-                    predecessors.add(neighbor);
-                    queue.add(neighbor);
-                }
-            }
+            List<Integer> newVertices = graph.neighbours(unqueued).stream()
+                    .filter(neighbor -> !visitArray[neighbor])
+                    .peek(neighbor -> {
+                        visitArray[neighbor] = true;
+                        distances.set(neighbor, distances.get(unqueued) + 1);
+                        predecessors.add(neighbor);
+                        queue.add(neighbor);
+                    }).toList();
+
+            logger.log(newVertices);
         }
         return new ImmutablePair<>(distances, predecessors);
-    }
-
-    private static void log(int level, List<Integer> found) {
-        System.out.println(level + ": " + found.stream()
-                .map(String::valueOf)
-                .collect(Collectors.joining(",")));
     }
 
 }
