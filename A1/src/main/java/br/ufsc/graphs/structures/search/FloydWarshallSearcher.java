88b9574e1245b2829db45bd6520520ad1628921c
anthongretter <anthonporathgretter@yahoo.com.br>
2023-04-27 10:12:25 -0300

    versão inicial de Floyd-Warshall
    * adequa método weight na implemetação de grafo ponderado
    * adequa teste de grafo ponderado para suportar a mudença acima
    * ta nojento esse floyd-warshall hein

--- A1/src/main/java/br/ufsc/graphs/structures/search/FloydWarshallSearcher.java
+++ A1/src/main/java/br/ufsc/graphs/structures/search/FloydWarshallSearcher.java
@@ -1,28 +1,53 @@
 package br.ufsc.graphs.structures.search;
 
-import br.ufsc.graphs.structures.Graph;
 import br.ufsc.graphs.structures.WeightedGraphImp;
-import br.ufsc.graphs.structures.storage.MatrixStorage;
 
 import java.util.Arrays;
-import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class FloydWarshallSearcher {
 
-//    public static final double POSITIVE_INFINITY = Infinity;
+    public static Number[][] search(WeightedGraphImp g) {
+        Number[][] costs = buildInitialMatrix(g);
+        int n = g.getVerticesQnt();
 
-    public static int[][] search(Graph g) {
-        int[][] matrix = buildInitialMatrix(g);
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    Number altCost = addCosts(costs[i][k], costs[k][j]);
+                    if (altCost.doubleValue() < costs[i][j].doubleValue()) {
+                        costs[i][j] = altCost;
+                    }
+                }
+            }
+        }
+        return costs;
+    }
 
-        // TODO
+    private static Number addCosts(Number a, Number b) {
+        if (a.doubleValue() == Double.POSITIVE_INFINITY || b.doubleValue() == Double.POSITIVE_INFINITY) {
+            return Double.POSITIVE_INFINITY;
+        }
+        return a.doubleValue() + b.doubleValue();
+    }
 
+    private static Number[][] buildInitialMatrix(WeightedGraphImp g) {
+        int n = g.getVerticesQnt();
+        Number[][] matrix = new Number[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                Number cost = g.weight(i, j);
+                matrix[i][j] = i == j ? 0 : cost;
+            }
+        }
         return matrix;
     }
 
-    private static int[][] buildInitialMatrix(Graph g) {
-        return new int[][]{};
+    public static void printResult(Number[][] m) {
+        IntStream.range(0, m.length)
+                .mapToObj(index -> String.format("%d:%s", index+1, Arrays.stream(m[index]).map(String::valueOf).collect(Collectors.joining(","))))
+                .forEach(System.out::println);
     }
 
 }
